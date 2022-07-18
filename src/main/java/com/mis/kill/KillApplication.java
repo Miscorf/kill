e93861366109b44fa2cd1e9e074d52c052c2201b
tyler <1291571584@qq.com>
2022-07-18 20:22:40 +0800

    1.0 demo run

--- src/main/java/com/mis/kill/KillApplication.java
+++ src/main/java/com/mis/kill/KillApplication.java
@@ -1,9 +1,11 @@
 package com.mis.kill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mis.kill.pojo")
 public class KillApplication {
 
     public static void main(String[] args) {
